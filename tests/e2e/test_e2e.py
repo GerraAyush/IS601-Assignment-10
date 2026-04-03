ceeb51df67e58ae8fb82366dbd3e7e96018c0edf
GerraAyush <ayushgerra3@gmail.com>
2026-04-02 20:21:56 -0400

    added fastapi server and tests

---
+++ tests/e2e/test_e2e.py
@@ -0,0 +1,31 @@
+import pytest
+from playwright.sync_api import expect
+
+
+@pytest.mark.e2e
+def test_hello_world(page, fastapi_server):
+    page.goto('http://localhost:8001')
+    
+    assert page.inner_text('h1') == 'Hello World'
+
+
+@pytest.mark.e2e
+def test_calculator_add(page, fastapi_server):
+    page.goto("http://localhost:8001")
+
+    page.fill("#a", "10")
+    page.fill("#b", "5")
+    page.click('button:text("Add")')
+
+    expect(page.locator("#result")).to_have_text("Result: 15")
+
+
+@pytest.mark.e2e
+def test_calculator_divide_by_zero(page, fastapi_server):
+    page.goto("http://localhost:8001")
+
+    page.fill("#a", "10")
+    page.fill("#b", "0")
+    page.click('button:text("Divide")')
+
+    expect(page.locator("#result")).to_have_text("Error: Cannot divide by zero!")
